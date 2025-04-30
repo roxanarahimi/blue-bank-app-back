ea668a490ab64e1460581688a9370eee52dd0d1b
Roxana <ms.roxanarahimi@gmail.com>
2025-04-30 18:05:55 +0330

    add models

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -37,19 +37,19 @@ class MainController extends Controller
                 $dat = Tour::orderByDESC('TourID')
                     ->where('State', 2)
                     ->whereDate('StartDate','>=', date(today()->subDays(5)))
-//                    ->whereHas('TourAssignmentItem', function ($z) use ($request) {
-//                        $z->whereHas('Assignment', function ($x) use ($request) {
-//                            $x->whereHas('Transporter', function ($y) use ($request) {
-//                                $y->WhereHas('Party');
-//                            });
-//                        });
-//                    })
-//                    ->whereHas('invoices', function ($q) use ($request) {
-//                        $q->whereHas('order', function ($d) {
-//                            $d->whereHas('orderItems');
-//                        });
-//
-//                    })
+                    ->whereHas('TourAssignmentItem', function ($z) use ($request) {
+                        $z->whereHas('Assignment', function ($x) use ($request) {
+                            $x->whereHas('Transporter', function ($y) use ($request) {
+                                $y->WhereHas('Party');
+                            });
+                        });
+                    })
+                    ->whereHas('invoices', function ($q) use ($request) {
+                        $q->whereHas('order', function ($d) {
+                            $d->whereHas('orderItems');
+                        });
+
+                    })
 
                     ->where('FiscalYearRef', 1405)
                     ->paginate(100);
