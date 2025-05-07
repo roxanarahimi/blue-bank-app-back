658170cea1785a3d43a484e6f20e9164fe6ea437
Roxana <ms.roxanarahimi@gmail.com>
2025-05-07 15:25:41 +0330

    commit

--- app/Models/Broker.php
+++ app/Models/Broker.php
@@ -18,14 +18,17 @@ class Broker extends Model
     {
         return $this->hasMany(Assignment::class,  'BrokerRef','BrokerID')
             ->whereHas('TourAssignmentItem',function ($a){
-                $a->whereHas('Tour',function ($t){
+                $a->whereHas('Tour');
+                $a->with('Tour');
+            });
+////            ->whereHas('TourAssignmentItem',function ($a){
+//                $a->whereHas('Tour',function ($t){
 //                    $t->where('State', 2);
 //                    $t->whereDate('StartDate', date(today()));
-                });
-            })
+//                });
+//            })
 //            ->with('TourAssignmentItem',function ($q){
 //                $q->with('Tour');
-//            })
-            ;
+//            });
     }
 }
